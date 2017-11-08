fbd7b38fc81d19134eef1c10d0a22ecba6b905a1
Anastasia Sorokina <nastusha_sorokina@mail.ru>
2017-11-08 13:29:17 +0300

    Убрала лишний слэш

--- src/app/doctor/doctor.routing.ts
+++ src/app/doctor/doctor.routing.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DoctorCabinetComponent } from './doctor-cabinet/doctor-cabinet.component';
 
 const routes: Routes = [
-  { path: '/cabinet/:id', component: DoctorCabinetComponent },
+  { path: 'cabinet/:id', component: DoctorCabinetComponent },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
